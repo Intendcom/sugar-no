d4414e0298d9aa45162cf7cf683bf419876a91a9
juris.pukitis <juris.pukitis@neotech.lv>
2024-12-19 11:55:20 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function setCookieAcceptance(event) {
+    event.preventDefault();
+    localStorage.setItem('cookiesAccepted', "true");
+    let elementById = document.getElementById("cookies");
+    elementById.classList.remove("show");
+}
+
+function getCookieAcceptance() {
+    return localStorage.getItem('cookiesAccepted');
+}
+
+window.addEventListener("load", function () {
+    let cookieAcceptance = getCookieAcceptance();
+    if (cookieAcceptance == null) {
+        let elementById = document.getElementById("cookies");
+        elementById.classList.add("show");
+    }
+})
